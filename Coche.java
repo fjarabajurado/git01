2f6c09601b903233c83dfe735553a909b5e347a0
fjarabajurado <fj.jarabajurado@edu.gva.es>
2024-12-18 13:45:03 +0100

    Añado constructor de coche

--- Coche.java
+++ Coche.java
@@ -1,3 +1,11 @@
 public class Coche {
-    
+    String matricula;
+    int anyoMatricula;
+    int cilindrada;
+
+    public Coche(String a, int anyo, int cilin){
+        this.matricula = a;
+        this.anyoMatricula = anyo;
+        this.cilindrada = cilin;
+    }
 }
